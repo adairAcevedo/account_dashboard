700e8bd276e237fcbdb0e0a5fe436739ceab604e
Adair Acevedo <148427510+adairAcevedo@users.noreply.github.com>
2026-03-16 19:08:32 -0500

    fix: set dinamic dashboard title

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -94,7 +94,7 @@
 
     <header class="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-white">Dashboard</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-white">{{title}}</h1>
       </div>
     </header>
     <main>
@@ -105,10 +105,11 @@
 
 <script setup>
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 import { authStore } from '@/stores/authStore'
 import { useRouter } from 'vue-router'
 
+const props = defineProps(['title'])
 const router = useRouter();
 
 // cambiar la imagen por un icono
