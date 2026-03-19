19512cfb6a4b87c398a8578bb6824121d0377f26
Adair Acevedo <148427510+adairAcevedo@users.noreply.github.com>
2026-03-19 15:26:55 -0500

    fix: applied format to  movements componen

--- src/components/Movements.vue
+++ src/components/Movements.vue
@@ -1,27 +1,27 @@
 <template>
     <div class="mx-auto max-w-4xl px-4 py-6 sm:px-6 lg:px-8">
       <div class="grid grid-cols-12 gap-x-6 px-0 py-3 border-b border-gray-200 dark:border-white/10">
-    <div class="col-span-6 flex items-center gap-x-1 cursor-pointer group" @click="clickSort('description')">
-      <p class="text-xs font-semibold uppercase tracking-wider text-gray-900 dark:text-white group-hover:text-gray-500 dark:text-gray-400">Descripciòn</p>
-      <ArrowsUpDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field != 'description'" />
-      <ArrowDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='description' && sortList.direction =='desc'"/>
-      <ArrowUpIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='description' && sortList.direction =='asc'"/>
-    </div>
-    <div class="col-span-3 flex items-center gap-x-1 cursor-pointer group" @click="clickSort('created_at')">
-      <p class="text-xs font-semibold uppercase tracking-wider dark:text-white group-hover:text-gray-500 dark:text-gray-400">Fecha</p>
-      <ArrowsUpDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field != 'created_at'" />
-      <ArrowDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='created_at' && sortList.direction =='desc'"/>
-      <ArrowUpIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='created_at' && sortList.direction =='asc'"/>
+        <div class="col-span-6 flex items-center gap-x-1 cursor-pointer group" @click="clickSort('description')">
+          <p class="text-xs font-semibold uppercase tracking-wider text-gray-900 dark:text-white group-hover:text-gray-500 dark:text-gray-400">Descripciòn</p>
+          <ArrowsUpDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field != 'description'" />
+          <ArrowDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='description' && sortList.direction =='desc'"/>
+          <ArrowUpIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='description' && sortList.direction =='asc'"/>
+        </div>
+        <div class="col-span-3 flex items-center gap-x-1 cursor-pointer group" @click="clickSort('created_at')">
+          <p class="text-xs font-semibold uppercase tracking-wider dark:text-white group-hover:text-gray-500 dark:text-gray-400">Fecha</p>
+          <ArrowsUpDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field != 'created_at'" />
+          <ArrowDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='created_at' && sortList.direction =='desc'"/>
+          <ArrowUpIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='created_at' && sortList.direction =='asc'"/>
 
-    </div>
-    <div class="col-span-3 flex items-center gap-x-1 cursor-pointer group" @click="clickSort('amount')">
-      <p class="text-xs font-semibold uppercase tracking-wider text-gray-900 dark:text-white group-hover:text-gray-500 dark:text-gray-400">Monto</p>
-      <ArrowsUpDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field != 'amount'"/>
-      <ArrowDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='amount' && sortList.direction =='desc'"/>
-      <ArrowUpIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='amount' && sortList.direction =='asc'"/>
+        </div>
+        <div class="col-span-3 flex items-center gap-x-1 cursor-pointer group" @click="clickSort('amount')">
+          <p class="text-xs font-semibold uppercase tracking-wider text-gray-900 dark:text-white group-hover:text-gray-500 dark:text-gray-400">Monto</p>
+          <ArrowsUpDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field != 'amount'"/>
+          <ArrowDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='amount' && sortList.direction =='desc'"/>
+          <ArrowUpIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="sortList.field =='amount' && sortList.direction =='asc'"/>
 
-    </div>
-  </div>
+        </div>
+      </div>
       <div>
           <ul role="list" class="divide-y divide-white/5" v-for="movement in movements" :key="movement.uid">
             <li class="grid grid-cols-12 gap-x-6 py-5 items-center gap-x-6 py-5 hover:bg-gray-700" >
@@ -44,7 +44,6 @@
     </div>
 </template>
 <script setup>
-// import {ref} from 'vue'
 const props = defineProps(['movements', 'sortList', 'isLoading'])
 const emit = defineEmits(['changeSort']);
 import { centsToUnits } from '@/utils/methods';
