4a4ed38821f1c10b977c0c788f2140325236f5e2
Adair Acevedo <148427510+adairAcevedo@users.noreply.github.com>
2026-04-03 15:03:29 -0500

    lang: add pagination translate

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -4,25 +4,25 @@
       <!-- PAGINATION -->
         <div class="flex items-center justify-between border-t border-white/10 px-4 py-3 sm:px-6">
         <div class="flex flex-1 justify-between sm:hidden">
-            <button @click="prevPage()" :disabled="paginate.page == 1" class="relative inline-flex items-center rounded-md border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium text-gray-200 hover:bg-white/10 disabled:opacity-75">Previous</button>
-            <button  @click="nextPage()" v-if="paginate.totalPages > paginate.page" class="relative ml-3 inline-flex items-center rounded-md border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium text-gray-200 hover:bg-white/10">Next</button>
+            <button @click="prevPage()" :disabled="paginate.page == 1" class="relative inline-flex items-center rounded-md border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium text-gray-200 hover:bg-white/10 disabled:opacity-75">{{t('paginator.previous')}}</button>
+            <button  @click="nextPage()" v-if="paginate.totalPages > paginate.page" class="relative ml-3 inline-flex items-center rounded-md border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium text-gray-200 hover:bg-white/10">{{ t('paginator.next') }}</button>
         </div>
         <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
             <div>
             <p class="text-sm text-gray-300">
-                Showing
+                {{t('paginator.show')}}
                 <span class="font-medium">{{ countItems.prevItems }}</span>
-                to
+                {{ t('paginator.to') }}
                 <span class="font-medium">{{ countItems.currentTotal }}</span>
-                of
+                {{ t('paginator.of') }}
                 <span class="font-medium">{{ paginate.totalItems }}</span>
-                results
+                {{ t('paginator.results') }}
             </p>
             </div>
             <div>
             <nav aria-label="Pagination" class="isolate inline-flex -space-x-px rounded-md">
                 <button @click="prevPage()" v-if="paginate.page > 1" class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 inset-ring inset-ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0 disabled:opacity-75">
-                    <span class="sr-only">Previous 2</span>
+                    <span class="sr-only">{{ t('paginator.previous') }}</span>
                     <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" class="size-5">
                         <path d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" fill-rule="evenodd" />
                     </svg>
@@ -34,7 +34,7 @@
                 class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-200 inset-ring inset-ring-gray-700 focus:z-20 focus:outline-offset-0">{{ page }}</button>
 
                 <button @click="nextPage()" v-if="paginate.totalPages > paginate.page" class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 inset-ring inset-ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0">
-                    <span class="sr-only">Next </span>
+                    <span class="sr-only">{{ t('paginator.next') }} </span>
                     <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" class="size-5">
                         <path d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" fill-rule="evenodd" />
                     </svg>
@@ -52,7 +52,8 @@
 import {computed} from 'vue'
 const props = defineProps(['paginate', 'itemsCount', 'class'])
 const emit = defineEmits(['changePaginate'])
-const space = "..."
+import { useI18n } from 'vue-i18n'
+const {t} = useI18n()
 const countItems = computed(() => {
     if (props.paginate.totalPages == props.paginate.page){
         return {
