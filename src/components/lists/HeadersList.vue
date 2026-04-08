491ca0980966f7cb75930412622a063e1ee64295
Adair Acevedo <148427510+adairAcevedo@users.noreply.github.com>
2026-04-08 14:26:05 -0500

    feat: add header list componente to sort field

---
+++ src/components/lists/HeadersList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :class="class" class="flex items-center gap-x-1 cursor-pointer group" @click="sort(fieldName)">
+          <p class="text-sm font-semibold tracking-wider text-gray-900 dark:text-white group-hover:text-gray-500 dark:text-gray-400">{{ textHeader }} </p>
+          <ArrowsUpDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="fieldOrder != fieldName" />
+          <ArrowDownIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="fieldOrder == fieldName && fieldDirection =='desc'"/>
+        <ArrowUpIcon class="size-3.5 dark:text-white group-hover:text-gray-900 dark:group-hover:text-gray-500" v-if="fieldOrder == fieldName && fieldDirection =='asc'"/>
+    </div>
+</template>
+<script setup>
+import {ArrowsUpDownIcon, ArrowUpIcon, ArrowDownIcon} from '@heroicons/vue/24/outline'
+
+const props = defineProps(['class', 'fieldOrder', 'fieldName', 'textHeader', 'fieldDirection' ])
+const emit = defineEmits(['clickHeaderSort'])
+
+const sort = (field) => {
+    emit('clickHeaderSort', field)
+}
+
+</script>
